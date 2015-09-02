23c23b4b2f8c743dda9a6baff1c7c7464eb2a56b
wm <sdhz_wm@163.com>
2015-09-02 16:37:01 +0800

    Initial Commit

---
+++ DouBan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DouBan
+//
+//  Created by 王蒙 on 15/9/2.
+//  Copyright (c) 2015年 王蒙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DouBan/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DouBan
+//
+//  Created by 王蒙 on 15/9/2.
+//  Copyright (c) 2015年 王蒙. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
